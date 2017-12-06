f7edf3f4db5820270c84551a6fc22d6e2622c73c
sshong1991 <sshong1991@gmail.com>
2017-12-06 15:33:00 +0900

    task serializer validation, inventory update function complete

--- django_app/inventory/serializer/task.py
+++ django_app/inventory/serializer/task.py
@@ -6,3 +6,17 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+    def create(self, validated_data):
+        if validated_data["action"] == "ADD":
+            validated_data["inventory"].quantity += validated_data["value"]
+        else:
+            validated_data["inventory"].quantity -= validated_data["value"]
+        validated_data["inventory"].save()
+        return Task.objects.create(**validated_data)
+
+    def validate(self, data):
+        if data["action"] == "SUB":
+            if data["value"] > data["inventory"].quantity:
+                raise serializers.ValidationError("Value must be Smaller then quatities")
+        return data
